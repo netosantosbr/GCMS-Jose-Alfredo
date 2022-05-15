d1b0521e378ee0d0da73ba4f5e797450fec52f1b
José Alfredo <netoalfredo9@gmail.com>
2022-05-15 00:46:18 -0300

    Second commit.

--- GCMS Pratica Git/src/HelloWorld.java
+++ GCMS Pratica Git/src/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World!");
 		System.out.println("First change.");
+		System.out.println("Second change.");
 	}
 
 }
