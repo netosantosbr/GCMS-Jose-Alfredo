f1bc685c02e20d84e72321795778f551947cdc25
José Alfredo <netoalfredo9@gmail.com>
2022-08-11 02:41:23 -0300

    agora sim, conflitos resolvidos!

--- GCMS Pratica Git/src/HelloWorld.java
+++ GCMS Pratica Git/src/HelloWorld.java
@@ -6,16 +6,13 @@ public class HelloWorld {
 		System.out.println("Hello World!");
 		System.out.println("First change.");
 		System.out.println("Second change.");
-<<<<<<< HEAD
 		
 		
 		System.out.println("Resolvido o problema.");
 		System.out.println("Alterando os mesmos trechos do passo anterior(passo 2)");
 		System.out.println("Vai funcionar!!(ainda passo 2)");
-=======
 		System.out.println("1 - Commit realizado na branch Hotfix!");
 		System.out.println("1.1 - Este é o passo 2.");
->>>>>>> hotfix
 	}
 
 }
