9d3c7512aaf3124a33aee950dc019f4b2f431614
José Alfredo <netoalfredo9@gmail.com>
2022-08-11 02:32:08 -0300

    conflitos resolvidos

--- GCMS Pratica Git/src/HelloWorld.java
+++ GCMS Pratica Git/src/HelloWorld.java
@@ -6,6 +6,11 @@ public class HelloWorld {
 		System.out.println("Hello World!");
 		System.out.println("First change.");
 		System.out.println("Second change.");
+		
+		
+		System.out.println("Resolvido o problema.");
+		System.out.println("Alterando os mesmos trechos do passo anterior(passo 2)");
+		System.out.println("Vai funcionar!!(ainda passo 2)");
 	}
 
 }
